ee5441be4f943f3eca873da3ca6cb271d2621b23
gitanjali <102031431+kotkarashvini@users.noreply.github.com>
2022-04-04 12:17:57 +0530

    update diff.java

--- diff.java
+++ diff.java
@@ -2,5 +2,7 @@ public class diff {
     public static void main(String[] args) {
         System.out.println("2 file");
         System.out.println("commit");
+        System.out.println("in git hub changes r done in diff.java");
+        
     }
 }
