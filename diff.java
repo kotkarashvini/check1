e61cf199d7e5f9841411f2b17d3a89ce569209bf
gitanjali <maligitanjali123@gmail.com>
2022-04-04 12:21:45 +0530

    update diff.java

--- diff.java
+++ diff.java
@@ -2,5 +2,6 @@ public class diff {
     public static void main(String[] args) {
         System.out.println("2 file");
         System.out.println("commit");
+        System.out.println("in 22222222 added fro, visula stuuasbajs");
     }
 }
